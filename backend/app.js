23819ea6248bd81e571a1edb1b8ec660573b295e
Keyvan <95883365+key7an@users.noreply.github.com>
2023-02-18 13:21:34 +0330

    PUT and PATCH added to cors

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
