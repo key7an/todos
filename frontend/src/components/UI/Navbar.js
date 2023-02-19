e4263c9eefac05c1a1b69eecf2dee9846fdd4009
Keyvan <95883365+key7an@users.noreply.github.com>
2023-02-19 10:45:59 +0330

    Update Navbar.js

--- frontend/src/components/UI/Navbar.js
+++ frontend/src/components/UI/Navbar.js
@@ -7,7 +7,9 @@ const Navbar = () => {
     <div className="relative h-10 mb-5 border-b-2 border-sky-400 bg-sky-200">
       <div className="flex justify-between p-2">
         <PassedTodos />
-        <h1 className=" text-[20px] font-semibold translate-x-3">Todo App</h1>
+        <h1 className=" text-[24px] font-semibold -mt-2 translate-x-3">
+          Todo App
+        </h1>
         <UpcomingTodos />
       </div>
     </div>
